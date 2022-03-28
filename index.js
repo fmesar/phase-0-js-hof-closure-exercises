0ec42d07e544911659778055959e6ce3e3de04b1
Francisco Mesar <fmesar87@gmail.com>
2022-03-27 23:53:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
-function forEach() {
+function forEach(array, callback) {
+  array.forEach(callback)
+}
+
+function map(array, callback) {
+
+ const newArray = array.map(callback)
+  return newArray
 
 }
 
-function map() {
+function filter(array, callback) {
+  const newArray = array.filter(callback)
+  return newArray
 
 }
 
-function filter() {
-
+function every(array, callback) {
+  const newArray = array.every(callback)
+  return newArray
 }
 
-function every() {
-
+function some(array, callback) {
+  const newArray = array.some(callback)
+  return newArray
 }
 
-function some() {
-
+function majority(array, callback) {
+if(callback(array) != true){
+  return true
+}else {return false}
 }
 
-function majority() {
-
+function once(callback) {
+  // let initialize = _.once(callback)
+  // return function (){
+  //   callback();
+  //   initialize();
+  // }
 }
 
-function once() {
+function groupBy(array, callback) {
+  // const newArray= _.groupBy(array, callback);
+  // const obj = Object.fromEntries(newArray);
+  const obj = Object.assign({}, array);
+   return obj
 
+//  const result = Object.keys(array).map(callback())
+
+//  return result
 }
 
-function groupBy() {
+function arrayToObject(array,collback) {
 
-}
+  // const obj = Object.assign(collback, array);
+// return obj.
 
-function arrayToObject() {
-
+// const obj = Object.assign({}, array);
+// Object.entries(obj).forEach(entry => {
+//   let key = entry[0]
+//   let value = entry[1]
+//   obj[key]= value
+// })
+// return obj
 }
 
 module.exports = { 
